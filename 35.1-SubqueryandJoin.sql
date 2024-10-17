d72609f9fcc6c0beecb9a51fdd430a66dd57f7f6
cgrzmustafa <cigersizmustafa5@gmail.com>
2024-10-17 19:14:56 +0300

    dosyalar yuklendi

---
+++ 35.1-SubqueryandJoin.sql
@@ -0,0 +1,10 @@
+-- Kitap sayfası sayısı ortalama kitap sayfası sayısıdan fazla
+-- olan kitap isimlerini, yazar isim ve soyisimleri ile birlikte sıralayalım.
+
+SELECT author.first_name, author.last_name, book.title, book.page_number
+FROM author
+INNER JOIN book ON book.author_id = author.id
+WHERE page_number <
+(
+	SELECT AVG(page_number) FROM book
+);
